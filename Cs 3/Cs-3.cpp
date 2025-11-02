4e3d6943a7e5e277f4068d4b320bee493ea20a45
Srisuban <srisuban04@gmail.com>
2025-11-02 14:22:04 +0530

    Cs-3-codes

---
+++ Cs 3/Cs-3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n,num,digit,rev=0;
+    cout<<"Enter a postive number to reverse:";
+    cin>>num;
+    n=num;
+     while (num)
+     {
+       digit=num%10;
+       rev=(rev*10)+digit;
+       num=num/10;
+     }
+     cout<<"The reverse of the  number is :"<<rev<<endl;
+     if(n==rev)
+     {
+       cout<< "The number is a palindrome";
+     }
+     else 
+      {
+        cout<<"The number is not a palindrome";
+      }
+}
